fb02a002aa831bc7bea805323829b90e134479d4
JoanBecerril <90915741+JoanBecerril@users.noreply.github.com>
2023-11-20 20:21:42 +0100

    20/11/2023_JoanBeccerril
    index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/home.css">
+</head>
+
+<body>
+    <header id="header">
+        <a href="./index.php"><img id="logo" src="./src/LOGO3.png" alt="logo"></a>
+        <a href="" class="secciones">
+            <p>Terraza</p>
+        </a>
+        <a href="" class="secciones">
+            <p>Comedor</p>
+        </a>
+        <a href="" class="secciones">
+            <p>Sala privada</p>
+        </a>
+        <a href="" class="secciones">
+            <p>Cerrar sesión</p>
+        </a>
+    </header>
+</body>
+
+</html>
