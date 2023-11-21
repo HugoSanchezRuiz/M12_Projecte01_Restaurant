dc8fdbf8336750d0035951257a59e2f0976978b2
JoanBecerril <90915741+JoanBecerril@users.noreply.github.com>
2023-11-21 18:22:46 +0100

    20/11/2023_JoanBeccerril
    comedor2.php

---
+++ paginas-mesas/comedores/comedor2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/home.css">
+</head>
+
+<body>
+    <header>
+        <a href="./index.php"><img id="logo" src="./src/LOGO3.png" alt="logo"></a>
+        <a href="./paginas-mesas/terrazas/terraza1.php" class="secciones">
+            <p>Terrazas</p>
+        </a>
+        <a href="./paginas-mesas/comedores/comedor1.php" class="secciones">
+            <p>Comedores</p>
+        </a>
+        <a href="./paginas-mesas/salas-privadas/sala-privada1.php" class="secciones">
+            <p>Salas privadas</p>
+        </a>
+        <a href="" class="secciones">
+            <p>Cerrar sesión</p>
+        </a>
+    </header>
+    <div class="row flex" id="">
+        <div id="restaurante">
+            <p>hola</p>
+        </div>
+        <div id="historial">
+            <p>Hola</p>
+        </div>
+    </div>
+</body>
+</html>
