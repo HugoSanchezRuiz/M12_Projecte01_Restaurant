8918b928030c3c69717def96c20c813aead1757e
JoanBecerril <90915741+JoanBecerril@users.noreply.github.com>
2023-11-21 18:22:19 +0100

    20/11/2023_JoanBeccerril
    comedor1.php

--- paginas-mesas/comedores/comedor1.php
+++ paginas-mesas/comedores/comedor1.php
@@ -1,70 +1,186 @@
-<!DOCTYPE html>
-<html lang="en">
+* {
+    margin: 0;
+    padding: 0;
+    font-family: 'Lora', serif;
+}
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../../css/home.css">
-</head>
+html {
+    color: white;
+}
 
-<body>
-    <header>
-        <a href="../../index.php"><img id="logo" src="../../src/LOGO3.png" alt="logo"></a>
-        <a href="" class="secciones">
-            <p>Terrazas</p>
-        </a>
-        <a href="" class="secciones">
-            <p>Comedores</p>
-        </a>
-        <a href="" class="secciones">
-            <p>Salas privadas</p>
-        </a>
-        <a href="" class="secciones">
-            <p>Cerrar sesión</p>
-        </a>
-        <!-- COMEDORES -->
-        <div class="salas">
-            <a href="">
-                <p>Comedor 1</p>
-            </a>
-            <a href="">
-                <p>Comedor 2</p>
-            </a>
-            <a href="">
-                <p>Comedor 3</p>
-            </a>
-        </div>
-        <!-- --------- -->
-    </header>
-    <div class="row flex" id="">
-        <div id="restaurante">
-            <div class="column-2 comedor-1-2-mesa">
-                <img src="../../src/MESA-DE-6.png" class="img-mesas">
-            </div>
-            <div class="column-2 comedor-1-2-mesa">
-                <img src="../../src/MESA-DE-6.png" class="img-mesas">
-            </div>
-            <div class="column-2 comedor-1-2-mesa">
-                <img src="../../src/MESA-DE-6.png" class="img-mesas">
-            </div>
-            <div class="column-2 comedor-1-2-mesa">
-                <img src="../../src/MESA-DE-6.png" class="img-mesas">
-            </div>
-            <div class="column-2 comedor-1-2-mesa">
-                <img src="../../src/MESA-DE-6.png" class="img-mesas">
-            </div>
-            <div class="column-2 comedor-1-2-mesa">
-                <img src="../../src/MESA-DE-6.png" class="img-mesas">
-            </div>
-        </div>
-        <div id="historial">
-            <p>Hola</p>
-        </div>
-    </div>
-</body>
+/* HEADER */
 
-</html>
+header {
+    background-color: #0d2b33;
+    height: 10vh;
+    display: flex;
+    width: 100%;
+    align-items: center;
+}
+
+#logo {
+    margin: 0.4vh 1vh 0 2.5vh;
+    height: 8vh;
+}
+
+.secciones {
+    font-size: 2.2vh;
+    color: #fdf7ec;
+    text-align: right;
+    text-decoration: none;
+    font-weight: 500;
+    margin: 0 1.5vh;
+    transition: all 0.2s;
+}
+
+.secciones:hover {
+    border-bottom: 4px solid;
+    border-bottom-left-radius: 0.5vh;
+    border-bottom-right-radius: 0.5vh;
+}
+
+.secciones-secund {
+    font-size: 1.8vh;
+    color: #fdf7ec;
+    text-align: right;
+    text-decoration: none;
+    font-weight: 500;
+    margin: 0 1.5vh;
+    transition: all 0.2s;
+}
+
+.secciones-secund:hover {
+    border-bottom: 4px solid;
+    border-bottom-left-radius: 0.5vh;
+    border-bottom-right-radius: 0.5vh;
+}
+
+.salas {
+    margin-left: 25vh;
+}
+
+.seleccionado-ahora {
+    border-bottom: 4px solid;
+    border-bottom-left-radius: 0.5vh;
+    border-bottom-right-radius: 0.5vh;
+}
+
+/* HTML */
+
+#conjunto {
+    margin-top: 10vh;
+}
+
+#restaurante {
+    width: 80%;
+    height: 90vh;
+    background-color: #fdf7ec;
+}
+
+#historial {
+    width: 20%;
+    height: 90vh;
+    background-color: grey;
+}
+
+.img-mesas {
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+    margin: auto;
+}
+
+/* TERRAZAS */
+
+.terraza-mesa {
+    height: 45vh;
+}
+
+/* COMEDORES */
+
+.comedor-1-2-mesa {
+    height: 30vh;
+}
+
+.comedor-3-mesa {
+    height: 30vh;
+}
+
+/* SALAS PRIVADAS */
+
+.salaprivada-1-mesa {
+    height: 45vh;
+}
+
+.salaprivada-2-3-mesa {
+    height: 45vh;
+}
+
+.salaprivada-4-mesa {
+    height: 45vh;
+}
+
+/* ESPECIALES */
+
+.flex {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.row:after {
+    content: "";
+    display: table;
+    clear: both;
+}
+
+.column-1 {
+    width: 100%;
+    float: left;
+}
+
+.column-2 {
+    width: 50%;
+    float: left;
+}
+
+.column-3 {
+    width: 33.33%;
+    float: left;
+}
+
+.column-4 {
+    width: 25%;
+    float: left;
+}
+
+.column-5 {
+    width: 20%;
+    float: left;
+}
+
+.column-6 {
+    width: 16.66%;
+    float: left;
+}
+
+.column-75 {
+    width: 75%;
+    float: left;
+}
+
+/* RESPONSIVE */
+
+@media only screen and (max-width: 600px) {
+    .column-2 {
+        width: 50%;
+    }
+
+    .column-3 {
+        width: 50%;
+    }
+
+    .column-4 {
+        width: 50%;
+    }
+}
