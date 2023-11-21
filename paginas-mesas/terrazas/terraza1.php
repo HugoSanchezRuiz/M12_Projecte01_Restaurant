756d24db3a25af817bad7d86b919ee8ce9769564
JoanBecerril <90915741+JoanBecerril@users.noreply.github.com>
2023-11-21 17:15:05 +0100

    21/11/2023_JoanBecerril
    terraza1.php

---
+++ paginas-mesas/terrazas/terraza1.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../../css/home.css">
+</head>
+
+<body>
+    <header>
+        <a href="../../index.php"><img id="logo" src="../../src/LOGO3.png" alt="logo"></a>
+        <a href="" class="secciones">
+            <p>Terrazas</p>
+        </a>
+        <a href="" class="secciones">
+            <p>Comedores</p>
+        </a>
+        <a href="" class="secciones">
+            <p>Salas privadas</p>
+        </a>
+        <a href="" class="secciones">
+            <p>Cerrar sesión</p>
+        </a>
+    </header>
+    <div class="row flex" id="">
+        <div id="restaurante">
+            <div class="column-1 terraza-mesa">
+                <img src="../../src/MESA-DE-10.png" class="img-mesas">
+            </div>
+            <div class="column-1 terraza-mesa">
+                <img src="../../src/MESA-DE-10.png" class="img-mesas">
+            </div>
+        </div>
+        <div id="historial">
+            <p>Hola</p>
+        </div>
+    </div>
+</body>
+
+</html>
