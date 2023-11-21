9e012e223004a70c2f3f785df1aac5bc005da705
JoanBecerril <90915741+JoanBecerril@users.noreply.github.com>
2023-11-21 18:23:36 +0100

    20/11/2023_JoanBeccerril
    comedor2.php

--- paginas-mesas/comedores/comedor2.php
+++ paginas-mesas/comedores/comedor2.php
@@ -8,32 +8,63 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./css/home.css">
+    <link rel="stylesheet" href="../../css/home.css">
 </head>
 
 <body>
     <header>
-        <a href="./index.php"><img id="logo" src="./src/LOGO3.png" alt="logo"></a>
-        <a href="./paginas-mesas/terrazas/terraza1.php" class="secciones">
+        <a href="../../index.php"><img id="logo" src="../../src/LOGO3.png" alt="logo"></a>
+        <a href="../terrazas/terraza1.php" class="secciones">
             <p>Terrazas</p>
         </a>
-        <a href="./paginas-mesas/comedores/comedor1.php" class="secciones">
+        <a href="./comedor1.php" class="secciones seleccionado-ahora">
             <p>Comedores</p>
         </a>
-        <a href="./paginas-mesas/salas-privadas/sala-privada1.php" class="secciones">
+        <a href="../salas-privadas/sala-privada1.php" class="secciones">
             <p>Salas privadas</p>
         </a>
         <a href="" class="secciones">
             <p>Cerrar sesión</p>
         </a>
+        <!-- COMEDORES -->
+        <div class="salas flex">
+            <a href="./comedor1.php" class="secciones-secund">
+                <p>Comedor 1</p>
+            </a>
+            <a href="./comedor2.php" class="secciones-secund seleccionado-ahora">
+                <p>Comedor 2</p>
+            </a>
+            <a href="./comedor3.php" class="secciones-secund">
+                <p>Comedor 3</p>
+            </a>
+        </div>
+        <!-- --------- -->
     </header>
     <div class="row flex" id="">
         <div id="restaurante">
-            <p>hola</p>
+            <div class="column-2 comedor-1-2-mesa">
+                <img src="../../src/MESA-DE-6.png" class="img-mesas">
+            </div>
+            <div class="column-2 comedor-1-2-mesa">
+                <img src="../../src/MESA-DE-6.png" class="img-mesas">
+            </div>
+            <div class="column-2 comedor-1-2-mesa">
+                <img src="../../src/MESA-DE-6.png" class="img-mesas">
+            </div>
+            <div class="column-2 comedor-1-2-mesa">
+                <img src="../../src/MESA-DE-6.png" class="img-mesas">
+            </div>
+            <div class="column-2 comedor-1-2-mesa">
+                <img src="../../src/MESA-DE-6.png" class="img-mesas">
+            </div>
+            <div class="column-2 comedor-1-2-mesa">
+                <img src="../../src/MESA-DE-6.png" class="img-mesas">
+            </div>
         </div>
         <div id="historial">
             <p>Hola</p>
         </div>
     </div>
 </body>
+
 </html>
