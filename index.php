b2775e42d597a67b97123e81c248f71d61c5e58d
JoanBecerril <90915741+JoanBecerril@users.noreply.github.com>
2023-11-21 18:21:04 +0100

    20/11/2023_JoanBeccerril
    index.php

--- index.php
+++ index.php
@@ -8,25 +8,60 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./css/home.css">
+    <link rel="stylesheet" href="../../css/home.css">
 </head>
 
 <body>
-    <header id="header">
-        <a href="./index.php"><img id="logo" src="./src/LOGO3.png" alt="logo"></a>
-        <a href="" class="secciones">
-            <p>Terraza</p>
+    <header>
+        <a href="../../index.php"><img id="logo" src="../../src/LOGO3.png" alt="logo"></a>
+        <a href="./terraza1.php" class="secciones seleccionado-ahora">
+            <p>Terrazas</p>
         </a>
-        <a href="" class="secciones">
-            <p>Comedor</p>
+        <a href="../comedores/comedor1.php" class="secciones">
+            <p>Comedores</p>
         </a>
-        <a href="" class="secciones">
-            <p>Sala privada</p>
+        <a href="../salas-privadas/sala-privada1.php" class="secciones">
+            <p>Salas privadas</p>
         </a>
         <a href="" class="secciones">
             <p>Cerrar sesión</p>
         </a>
+        <!-- TERRAZAS -->
+        <div class="salas flex">
+            <a href="./terraza1.php" class="secciones-secund">
+                <p>Terraza 1</p>
+            </a>
+            <a href="./terraza2.php" class="secciones-secund">
+                <p>Terraza 2</p>
+            </a>
+            <a href="./terraza3.php" class="secciones-secund">
+                <p>Terraza 3</p>
+            </a>
+            <a href="./terraza4.php" class="secciones-secund seleccionado-ahora">
+                <p>Terraza 4</p>
+            </a>
+        </div>
+        <!-- --------- -->
     </header>
+    <div class="row flex" id="">
+        <div id="restaurante">
+            <div class="column-2 terraza-mesa">
+                <img src="../../src/MESA-DE-3.png" class="img-mesas">
+            </div>
+            <div class="column-2 terraza-mesa">
+                <img src="../../src/MESA-DE-3.png" class="img-mesas">
+            </div>
+            <div class="column-2 terraza-mesa">
+                <img src="../../src/MESA-DE-2.png" class="img-mesas">
+            </div>
+            <div class="column-2 terraza-mesa">
+                <img src="../../src/MESA-DE-2.png" class="img-mesas">
+            </div>
+        </div>
+        <div id="historial">
+            <p>Hola</p>
+        </div>
+    </div>
 </body>
 
 </html>
